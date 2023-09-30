ca9be7b93d5e86a34daab9e9d4070262e1123902
DESKTOP-LRU92KK\pc <caugioi.lr@gmail.com>
2023-09-30 16:16:08 +0700

    Add project files.

---
+++ 1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Viết chương trình C# in ra chữ sau: C#
+//Biên dịch, chạy và kiểm tra kết quả tệp biên dịch trong thư mục bin/debug của ứng dụng
+
+Console.WriteLine("    +++++++++               ++++     ++++");
+Console.WriteLine("   +++++++++++          +++++++++++++++++++");
+Console.WriteLine("  ++++     ++++        +++++++++++++++++++");
+Console.WriteLine(" ++++                    ++++      ++++");
+Console.WriteLine(" ++++                   ++++      ++++");
+Console.WriteLine("  ++++     ++++     +++++++++++++++++++++");
+Console.WriteLine("   ++++   ++++     +++++++++++++++++++++");
+Console.WriteLine("    +++++++++     +++++++++++++++++++++");
+Console.WriteLine("     +++++++        ++++      ++++");
